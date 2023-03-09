e3ebd7d1806afb140d81adedf900e2e222136e47
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-09 01:59:42 +0100

    feat(pyo3): add main.py

--- pymi-pyo3/main.py
+++ pymi-pyo3/main.py
@@ -1,7 +1,21 @@
-from pymi_pyo3 import sum_as_string
+import argparse
+
+from pymi_pyo3 import sum_as_string  # ignore
 
 def main():
-    print(sum_as_string(1,2))
+
+    # create the parser object
+    parser = argparse.ArgumentParser(description='Add two numbers but return them as a string.')
+
+    # add the arguments to the parser
+    parser.add_argument('num1', type=int, help='The first number to add.')
+    parser.add_argument('num2', type=int, help='The second number to add.')
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    print(sum_as_string(args.num1, args.num2))
+
 
 if __name__ == "__main__":
     main()
